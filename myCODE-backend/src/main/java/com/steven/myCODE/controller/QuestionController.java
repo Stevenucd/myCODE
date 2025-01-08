273105ed298638f65c6e23c25dfec110b8559a86
Stevenucd <cuixiaozhang@outlook.com>
2025-01-08 17:11:09 +0000

    controller and service basic use update

--- myCODE-backend/src/main/java/com/steven/myCODE/controller/QuestionController.java
+++ myCODE-backend/src/main/java/com/steven/myCODE/controller/QuestionController.java
@@ -1,5 +1,6 @@
 package com.steven.myCODE.controller;
 
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.steven.myCODE.annotation.AuthCheck;
 import com.steven.myCODE.common.BaseResponse;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * question接口
@@ -218,6 +220,10 @@ public class QuestionController {
         // todo 在此处将实体类和 DTO 进行转换
         Question question = new Question();
         BeanUtils.copyProperties(questionEditRequest, question);
+        List<String> tags = questionEditRequest.getTags();
+        if (tags != null) {
+            question.setTags(JSONUtil.toJsonStr(tags));
+        }
         // 数据校验
         questionService.validQuestion(question, false);
         User loginUser = userService.getLoginUser(request);
